7383ff5dff077e3636dd56a9e89a0a1ea6be5f92
Sharada Kasam <sharadakasam@outlook.com>
2023-04-12 09:27:28 +0530

    Shopping edit add functionality

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { ingredient } from 'src/app/shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
+  @Output() IngredientAdded = new EventEmitter<ingredient>();
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new ingredient(ingName, ingAmount);
+    this.IngredientAdded.emit(newIngredient);
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,8 +7,12 @@ import { ingredient } from '../shared/Ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-  ingredients: ingredient[] =[
+  ingredients: ingredient[] = [
     new ingredient("Apples", 5),
     new ingredient("Tomatoes", 10)
   ];
+
+  onIngredientAdded(ingredient: ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
