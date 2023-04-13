75004710581e9b25cf7fe27d57861e9549a01ee4
Sharada Kasam <sharadakasam@outlook.com>
2023-04-13 08:14:23 +0530

    hostlister - document click

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,4 +8,9 @@ export class DropdownDirective {
     @HostListener('click') toggleOpen() {
         this.isOpen = !this.isOpen;
     }
+    //If we want the dropdown to be closed by anywhere on document click. 
+    //Remember to add elRef to constructor property
+    // @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    //     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    //   }
 }
