92058acf9e4ca88e45fc51f358c026aaaa31b41e
Sharada Kasam <sharadakasam@outlook.com>
2023-04-16 10:28:21 +0530

    Emit recipe from recipe service and emit selected one from recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,13 +8,13 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [];
   constructor(private recipeService: RecipeService) { }
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
@@ -8,9 +8,17 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipes.component.css'],
   providers: [RecipeService]
 })
-export class RecipesComponent {
-  selectedRecipe: Recipe ;
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
 
+  constructor(private recipeService: RecipeService) { }
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );  
+  }
   onRecipeWasSelected(event: Recipe) {
     this.selectedRecipe = event;
   }
