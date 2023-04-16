4e7cbcfec16c81a1bdeb3c19a8802d2df524fe08
Sharada Kasam <sharadakasam@outlook.com>
2023-04-16 11:04:23 +0530

    Added ingredients to Recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,10 +1,14 @@
+import { ingredient } from "../shared/Ingredient.model";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
-    constructor(name: string, desc: string, imagePath: string) {
+    public ingredients: ingredient[];
+    constructor(name: string, desc: string, imagePath: string, ingredients: ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients= ingredients;
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,23 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { ingredient } from "../shared/Ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg')
+        new Recipe('A Test Recipe', 'This is simply a test', 
+        'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg',
+        [
+            new ingredient('Meat',1),
+            new ingredient('Oil',1)
+        ]),
+        new Recipe('Another Test Recipe', 'This is simply a test', 
+        'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg', 
+        [
+            new ingredient('chicken',1),
+            new ingredient('Oil',1)
+        ])
     ];
 
     getRecipes(){
