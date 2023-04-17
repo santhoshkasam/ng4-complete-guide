6dfea42ad35fa51654e162647d067d006575d6de
Sharada Kasam <sharadakasam@outlook.com>
2023-04-17 07:10:25 +0530

    recipes child routes, accessing id value by subscribing to route params

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+export class RecipeDetailComponent implements OnInit {
+  // @Input() recipe: Recipe;
+  id: number;
+  recipe: Recipe;
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+
+    this.route.params.subscribe(
+      (params: Params) => { 
+        this.id = +params['id']; 
+        this.recipe = this.recipeService.getRecipe(id);
+      }
+    )
+  }
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,25 +8,27 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 
-        'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg',
-        [
-            new ingredient('Meat',1),
-            new ingredient('Oil',1)
-        ]),
-        new Recipe('Another Test Recipe', 'This is simply a test', 
-        'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg', 
-        [
-            new ingredient('chicken',1),
-            new ingredient('Oil',1)
-        ])
+        new Recipe('A Test Recipe', 'This is simply a test',
+            'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg',
+            [
+                new ingredient('Meat', 1),
+                new ingredient('Oil', 1)
+            ]),
+        new Recipe('Another Test Recipe', 'This is simply a test',
+            'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2-853x1024.jpg',
+            [
+                new ingredient('chicken', 1),
+                new ingredient('Oil', 1)
+            ])
     ];
-    constructor(private shoppingListService:ShoppingListService){}
-    getRecipes(){
+    constructor(private shoppingListService: ShoppingListService) { }
+    getRecipes() {
         return this.recipes.slice();//This will create a new array with exact copy like Recipe
     }
-
-    addIngredientsToShoppingList(ingredients:ingredient[]){
+    getRecipe(id: number) {
+        return this.recipes.slice()[id];
+    }
+    addIngredientsToShoppingList(ingredients: ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
     }
 }
